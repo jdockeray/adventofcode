71952d26bc7a6b9d5adc00d1547c7e4fa7c2f568
James Dockeray <james.dockeray@red-badger.com>
2020-12-24 11:29:01 +0000

    day 4

---
+++ pkg/day5/day5.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/jdockeray/adventofcode/pkg/day2"
+	"strconv"
+	"strings"
+)
+
+func ConvertToBinary(z string, o string) func(str string) string {
+	return func(str string) string {
+		s := strings.ReplaceAll(str, z, "0")
+		s = strings.ReplaceAll(s, o, "1")
+		return s
+	}
+}
+
+var convertRow = ConvertToBinary("F", "B")
+var convertCol = ConvertToBinary("L", "R")
+
+func ParseRowCols(rowcols string) (row string, col string) {
+	row = rowcols[0:7]
+	col = rowcols[7:]
+	return row, col
+}
+
+func n(s string) int {
+	i, _ := strconv.ParseInt(s, 2, len(s)+1)
+	return int(i)
+}
+
+func GetId(rowCols string) int {
+	row, col := ParseRowCols(rowCols)
+	r := n(convertRow(row))
+	c := n(convertCol(col))
+	return (r * 8) + c
+}
+
+func main() {
+	res, _ := day2.FetchData("http://127.0.0.1:8081/data/day5.txt")
+	defer res.Body.Close()
+	scanner := bufio.NewScanner(res.Body)
+	max := 0
+	min := 0
+	seats := map[int]bool{}
+
+	for scanner.Scan() {
+		str := scanner.Text()
+
+		id := GetId(str)
+		seats[id] = true
+		if max == 0 && min == 0 {
+			// first run of the loop we set max and min to the first value
+			max = id
+			min = id
+		}
+
+		if id > max {
+			max = id
+		}
+		if id < min {
+			min = id
+		}
+	}
+
+	for i := min; i <= max; i++ {
+		seat := seats[i]
+
+		if !seat {
+			fmt.Println("========")
+			fmt.Println("Your seat is:")
+			fmt.Println(i)
+			fmt.Println("========")
+		}
+	}
+
+	fmt.Println("The highest value is:")
+	fmt.Println(max)
+}
---
+++ pkg/day5/day5_test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetId(t *testing.T) {
+	type args struct {
+		rowCol string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"works",
+			args{
+				rowCol: "BFFFBBFRRR",
+			},
+			567,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetId(tt.args.rowCol); got != tt.want {
+				t.Errorf("GetId() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseRowCols(t *testing.T) {
+	type args struct {
+		rowcols string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantRow  string
+		wantCols string
+	}{
+		{
+			name: "works",
+			args: args{
+				rowcols: "BFFFBBFRRR",
+			},
+			wantRow:  "BFFFBBF",
+			wantCols: "RRR",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotRow, gotCols := ParseRowCols(tt.args.rowcols)
+			if gotRow != tt.wantRow {
+				t.Errorf("ParseRowCols() gotRow = %v, want %v", gotRow, tt.wantRow)
+			}
+			if gotCols != tt.wantCols {
+				t.Errorf("ParseRowCols() gotCols = %v, want %v", gotCols, tt.wantCols)
+			}
+		})
+	}
+}
+
+func TestConvertToBinary(t *testing.T) {
+	type args struct {
+		z   string
+		o   string
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "works",
+			args: args{
+				z:   "F",
+				o:   "B",
+				str: "BFFFBBF",
+			},
+			want: "1000110",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ConvertToBinary(tt.args.z, tt.args.o)(tt.args.str); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ConvertToBinary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
